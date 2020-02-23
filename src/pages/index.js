ce10aba0c76d41e1c0d11083d6c2a8da11f44ae8
Matt Canty <matthewcanty@gmail.com>
2020-02-23 19:09:07 +0000

    Remove email field

--- src/pages/index.js
+++ src/pages/index.js
@@ -176,16 +176,8 @@ class HomeIndex extends React.Component {
                   data-netlify="true"
                   action="/message-received"
                 >
-                  <div className="row uniform 50%">
-                    <div className="6u 12u$(xsmall)">
-                      <input
-                        type="text"
-                        name="name"
-                        id="name"
-                        placeholder="Name"
-                      />
-                    </div>
-                    <div className="6u 12u$(xsmall)">
+                  <div className="row uniform 100%">
+                    <div className="12u 12u$(xsmall)">
                       <input
                         type="email"
                         name="email"
