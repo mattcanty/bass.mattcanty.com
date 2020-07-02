e1b56b307ccfd074f8117c539dd311ee7a3a905c
Matt Canty <matthewcanty@gmail.com>
2020-07-02 21:26:09 +0100

    Add Dan Haywood gigs

--- src/data/Gigs.js
+++ src/data/Gigs.js
@@ -1,22 +1,61 @@
-const whoLinks = {
-  dgSolaris: 'https://dgsolaris.bandcamp.com/',
-  howsMyPop:
+const who = {
+  'DG Solaris': 'https://dgsolaris.bandcamp.com/',
+  'Hows My Pop?':
     'https://open.spotify.com/artist/6u80mPeDoYPHNrpfodi7Oj?si=w1chkmSPSqCgjqw8FIDSrg',
+  'Dan Haywood': 'http://danhaywood.net/',
 }
 
 const whereLinks = {
   paperdressVintage: 'https://paperdressvintage.co.uk/',
   glastonburyFestival: 'https://glastonburyfestivals.co.uk/',
+  brundellSocialClub: 'http://www.brudenellsocialclub.co.uk/',
 }
 
 export default [
   {
     notable: true,
     when: new Date('2007-06-22'),
-    who: 'Hows My Pop?',
-    whoLink: whoLinks.howsMyPop,
+    who: 'Hows My Pop',
     where: 'Glastonbury Festival',
-    whereLink: whereLinks.glastonburyFestival,
+    gigLink: 'https://www.bbc.co.uk/programmes/p00fkvms',
+  },
+  {
+    notable: true,
+    when: new Date('2014-12-10'),
+    who: 'Dan Haywood',
+    where: 'Brudenell Social Club',
+    gigLink: 'https://www.bbc.co.uk/programmes/p00fkvms',
+  },
+
+  {
+    notable: true,
+    when: new Date('2014-12-11'),
+    who: 'Dan Haywood',
+    where: 'Bethnal Green Working Mens Club',
+    gigLink: 'https://www.bbc.co.uk/programmes/p00fkvms',
+  },
+
+  {
+    notable: true,
+    when: new Date('2014-12-12'),
+    who: 'Dan Haywood',
+    where: 'The Cube, Bristol',
+    gigLink: 'https://www.bbc.co.uk/programmes/p00fkvms',
+  },
+
+  {
+    notable: true,
+    when: new Date('2014-12-13'),
+    who: 'Dan Haywood',
+    where: 'Kitchen Garden Cafe, Birmingham',
+    gigLink: 'https://www.bbc.co.uk/programmes/p00fkvms',
+  },
+
+  {
+    notable: true,
+    when: new Date('2014-12-14'),
+    who: 'Dan Haywood',
+    where: 'Ramsgate Music Hall',
     gigLink: 'https://www.bbc.co.uk/programmes/p00fkvms',
   },
 ]
